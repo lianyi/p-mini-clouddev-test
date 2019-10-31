dcf580426edf54c637966d787f1d5b2839a853a9
lianyihan <lianyihan@tencent.com>
2019-10-31 13:43:42 -0700

    minor

--- functions/add/index.js
+++ functions/add/index.js
@@ -17,7 +17,13 @@ exports.main2 = async (event, context) => {
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
   return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
     sum: event.a + event.b + 100
   }
 }
