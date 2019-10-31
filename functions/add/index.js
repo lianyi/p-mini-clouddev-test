935dc82332914f0991bbc4636a412e541a03edaf
lianyihan <lianyihan@tencent.com>
2019-10-31 14:45:36 -0700

    server code working

--- functions/add/index.js
+++ functions/add/index.js
@@ -1,7 +1,9 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
-cloud.init()
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+const db = cloud.database()
 
 // 云函数入口函数
 exports.main2 = async (event, context) => {
@@ -17,14 +19,22 @@ exports.main2 = async (event, context) => {
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-    sum: event.a + event.b + 100
+  try {
+    return await db.collection('todos').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        description: "learn cloud database",
+        due: new Date("2018-09-01"),
+        tags: [
+          "cloud",
+          "database"
+        ],
+        // 位置（113°E，23°N）
+        location: new db.Geo.Point(113, 23),
+        done: false
+      }
+    })
+  } catch (e) {
+    console.error(e)
   }
 }
