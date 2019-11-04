bd118c950b6fbac3a796d56aae1bb331556ee5e6
lianyihan <lianyihan@tencent.com>
2019-11-04 16:01:18 +0800

    update the accesstoken getter

--- functions/testAT/index.js
+++ functions/testAT/index.js
@@ -1,91 +1,90 @@
-//需求
+//////////////////
+// 设置需求      //
+//////////////////
 //1. 创建AccessTokenDb collection with one doc with id of ‘at’
-//2. 配置ENV for appid & secret
+//2. 配置ENV for APPID & SECRET
+//      process.env.APPID
+//      process.env.SECRET
 
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
 // 云函数入口文件
-const cloud = require('wx-server-sdk')
-const https = require('https')
+const cloud = require('wx-server-sdk');
+const https = require('https');
 
 //init cloud function before other cloud calls
 cloud.init({
-  env: 'lianyi-z6q87'//cloud.DYNAMIC_CURRENT_ENV
-})
+    env: 'lianyi-z6q87'//cloud.DYNAMIC_CURRENT_ENV
+});
 
-const db = cloud.database()
+const db = cloud.database();
+const tokenUrl = 'https://api.weixin.qq.com/cgi-bin/token?' +
+    'grant_type=client_credential&appid=' + process.env.APPID +
+    '&secret=' + process.env.SECRET;
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-    const wxContext = cloud.getWXContext()
-    console.info(await updateAccessToken('this is a token', process.env.TEST))
+    const wxContext = cloud.getWXContext();
+    let url = tokenUrl;
+    let eventuallySccessToken = '';
+    let accessToken = '';
+
+    const myAccessToken = await retrieveAccessToken();
+    // 检查数据库中是否有存储过accessToken记录
+    if (myAccessToken && myAccessToken.creat_time && myAccessToken.access_token) {
+        // 判断是否过期
+        let oldTime = myAccessToken.creat_time;
+        oldTime = new Date(oldTime).getTime();
+        let newTime = (new Date()).getTime();
+        let resutl = parseInt((newTime - oldTime) / 1000 / 60 / 60); //hours
+        if (resutl > 1) { //renew before 1 hour
+            // 从新获取access_token值
+            accessToken = await getHttpOption(url);
+            // 存储后台
+            if (accessToken) {
+                // 存储后台
+                await updateAccessToken(accessToken.access_token, (new Date()).toISOString());
+                eventuallySccessToken = accessToken.access_token
+            } else {
+                console.error({
+                    code: -1,
+                    msg: '获取失败！'
+                });
+                return -1;
+            }
+        } else {
+            // 没有过期 继续使用数据库中的access_token值
+            eventuallySccessToken = myAccessToken.access_token
+        }
+    } else {
+        // 从新请求并存储
+        accessToken = await getHttpOption(url)
+        // 插入数据库
+        if (accessToken) {
+            // 存储后台
+            await updateAccessToken(accessToken.access_token, (new Date()).toISOString());
+            eventuallySccessToken = accessToken.access_token
+        } else {
+            console.error({
+                code: -1,
+                msg: '获取失败！'
+            });
+            return -1;
+        }
+    }
+
     return {
         event,
-        token: await retrieveAccessToken(),
+        token: {
+            eventuallySccessToken,
+            msg: '获取成功！'
+        },
         openid: wxContext.OPENID,
         appid: wxContext.APPID,
         unionid: wxContext.UNIONID,
     }
-}
+};
 
-//
-// async function getAccessToken(ctx) {
-//     let url = 'https://api.weixin.qq.com/cgi-bin/token?' +
-//         'grant_type=client_credential&appid="你的addid"&secret=你的appsecret'
-//     let eventuallySccessToken = ''
-//     let accessToken = ''
-//     // wx_accessToken为存储accessToken的表
-//     const sqlAccessToken = await mysql('wx_accessToken').select('*').first()
-//
-//     // 数据库中是否有存储过accessToken记录
-//     if (sqlAccessToken !== undefined) {
-//         // 判断是否过期
-//         let oldTime = sqlAccessToken.creat_time
-//         oldTime = new Date(oldTime).getTime()
-//         let newTime = (new Date()).getTime()
-//         let resutl = parseInt((newTime - oldTime) / 1000 / 60 / 60)
-//         if (resutl > 1) {
-//             // 从新获取access_token值
-//             accessToken = await getHttpOption(url)
-//             // 存储后台
-//             if (accessToken) {
-//                 // 存储后台
-//                 await mysql('wx_accessToken').update({access_token: accessToken.access_token}).where('id', sqlAccessToken.id)
-//                 eventuallySccessToken = accessToken.access_token
-//             } else {
-//                 ctx.state.data = {
-//                     code: -1,
-//                     msg: '获取失败！'
-//                 }
-//                 return
-//             }
-//         } else {
-//             // 没有过期 继续使用数据库中的access_token值
-//             eventuallySccessToken = sqlAccessToken.access_token
-//         }
-//     } else {
-//         // 从新请求并存储
-//         accessToken = await getHttpOption(url)
-//         // 插入数据库
-//         if (accessToken) {
-//             // 存储后台
-//             await mysql('wx_accessToken').insert({access_token: accessToken.access_token})
-//             eventuallySccessToken = accessToken.access_token
-//         } else {
-//             ctx.state.data = {
-//                 code: -1,
-//                 msg: '获取失败！'
-//             }
-//             return
-//         }
-//     }
-//     ctx.state.data = {
-//         eventuallySccessToken,
-//         msg: '获取成功！'
-//     }
-// }
-//
-//
 
 // 自己封装的同步请求函数
 function getHttpOption(url) {
@@ -109,7 +108,7 @@ function getHttpOption(url) {
 // 自己封装的同步更新函数
 async function updateAccessToken(access_token, creat_time) {
     try {
-      return await db.collection('AccessTokenDb').doc('at').update({
+        return await db.collection('AccessTokenDb').doc('at').update({
             data: {
                 description: "latest accesstoken",
                 access_token: access_token,
